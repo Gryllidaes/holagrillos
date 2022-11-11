4cd0eae4c54386d9abf47b81a56942d690f770ed
Gryllidaes <Acostagrillo@outlook.com>
2022-11-11 07:24:35 +0100

    Muy feliz

--- Learn_Python/corazon.py
+++ Learn_Python/corazon.py
@@ -1,6 +1,6 @@
 #ALL THE VALUES
 
-x = int(input("definir primer valor: "))
-y = int(input("definir segundo valor: "))
+x = int(input("Definir primer valor: \n"))
+y = int(input("Definir segundo valor: \n"))
 
-print(x + y)
+print("\n\nResultado es:\n",x + y)
