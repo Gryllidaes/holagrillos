efa82dddbef8208d5b27502c9c6439d9e1dbc9b4
Gryllidaes <acostagrillo@outlook.com>
2023-01-04 21:42:11 +0100

    "SEGUNDO INTENTO"

--- Learn_Java/primer.java
+++ Learn_Java/primer.java
@@ -1,10 +1,10 @@
 // Your First Program
 
 class HelloWorld {
+    /**
+     * @param args
+     */
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
-    }
 
-    public HelloWorld() {
-    }
 }
